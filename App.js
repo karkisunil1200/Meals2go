7a659cdc76f1ab01b2b6cdeee62ef47aa2a631c3
sunilkarki <karkisunil1200@gmail.com>
2022-02-01 21:02:04 -0600

    Safebar view rendering for both IOS and Android

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import { StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  Platform,
+} from "react-native";
+
+const isAndroid = Platform.OS === "andorid";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView>
-        <View>
+      <SafeAreaView style={{ flex: 1, marginTop: isAndroid ? 50 : 0 }}>
+        <View style={{ padding: 16, backgroundColor: "red" }}>
           <Text>Our rect native blank canvas </Text>
         </View>
+        <View style={{ padding: 15, flex: 1, backgroundColor: "blue" }}>
+          <Text>List</Text>
+        </View>
       </SafeAreaView>
+      <StatusBar style="auto" />
     </>
   );
 }
