43096e77f63b5eaf344cc12fa580e10d9132b49e
sunilkarki <karkisunil1200@gmail.com>
2022-02-03 20:08:11 -0600

    Cleaned up the code

--- src/features/restaurants/components/restaurant-info.card.js
+++ src/features/restaurants/components/restaurant-info.card.js
@@ -1,47 +1,21 @@
 import React from "react";
-import { Image, View } from "react-native";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { Text } from "../../../components/typography/text.component";
 
-import { Card } from "react-native-paper";
-import styled from "styled-components/native";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
 
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]}
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const Info = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled(View)`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Address = styled(Text)`
-  font-family: ${(props) => props.theme.fonts.body}
-  font-size: ${(props) => props.theme.fontSizes.caption}
-`;
-
-const Section = styled(View)`
-  flex-direction: row;
-  align-items: center;
-`;
-const SectionEnd = styled(View)`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card.styles";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -75,11 +49,11 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
               {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
             </Spacer>
             <Spacer position="left" size="large">
-              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
-        <Address>2202 samuelpass hoover al</Address>
+        <Address>{address}</Address>
       </Info>
     </RestaurantCard>
   );
